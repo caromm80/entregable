879e499c277672067dded4f3cb2da6a170900fcd
caromm80 <caromm8@gmail.com>
2025-05-08 16:03:26 -0500

    repositorio uno

---
+++ src/validacionDePago.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class validacionDePago {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Validacion de pago");
+
+        // Definir el valor de la entrada
+        final int precioEntrada = 50000;
+
+        // Pedir al usuario que ingrese el monto pagado
+        System.out.print("Ingresa el monto pagado: ");
+        int montoPagado = sc.nextInt();
+
+        // Verificar si el pago es suficiente
+        if (montoPagado >= precioEntrada) {
+            System.out.println("Pago completo");
+        } else {
+            System.out.println("Pago insuficiente");
+        }
+
+
+
+        sc.close();
+
+    }
+}
